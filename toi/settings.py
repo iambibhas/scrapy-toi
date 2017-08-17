9d63f9995031d9dd9094c65ef297fe7598e720ca
Bibhas <iambibhas@gmail.com>
2017-08-17 15:45:55 +0530

    fixed spider for better dates

--- toi/settings.py
+++ toi/settings.py
@@ -89,3 +89,5 @@ DOWNLOADER_MIDDLEWARES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+CLOSESPIDER_ERRORCOUNT = 0
--- toi/spiders/article_spider.py
+++ toi/spiders/article_spider.py
@@ -2,10 +2,11 @@ import scrapy
 import itertools
 import datetime
 import calendar
+from dateutil import parser
 
 
 MONTHS = range(1, 13)
-YEARS = range(2002, 2017)
+YEARS = range(2001, 2018)
 BASE_DATE = datetime.date(1899, 12, 30)
 CURRENT_DATE = datetime.date.today()
 VALID_END_DATE = CURRENT_DATE - datetime.timedelta(days=2)
@@ -36,30 +37,33 @@ class ArticleSpider(scrapy.Spider):
                 yield scrapy.Request(url=article_url, callback=self.parse)
         elif 'articleshow' in response.url:
             # this is an article page
-            content = response.xpath('//div[@class="main-content"]')
-            title = content.xpath('//h1[@class="heading1"]/text()').extract_first()
+            title = response.xpath('//h1[@class="heading1"]/text()').extract_first()
 
             if not title:
                 raise Exception('No Title Found!')
 
-            published_dt = content.xpath('//span[@itemprop="datePublished"]/text()').extract_first()
+            published_dt = response.xpath('//span[@itemprop="datePublished"]/text()').extract_first()
 
             if not published_dt:
-                published_dt_text = content.xpath('//span[@class="time_cptn"]/text()').extract_first()
-                dt_parts = published_dt_text.split(' | ')
-                published_dt = dt_parts[-1]
+                published_dt_text = response.xpath('//span[@class="time_cptn"]/text()').extract_first()
+                if published_dt_text:
+                    dt_parts = published_dt_text.split(' | ')
+                    if len(dt_parts) > 0:
+                        published_dt = dt_parts[-1]
 
             if not published_dt:
-                published_dt_text = content.xpath('//span[@class="time_cptn"]//span/text()').extract()
-                published_dt = published_dt_text[-1]
+                dt_parts = response.xpath('//span[@class="time_cptn"]//span/text()').extract()
+                if len(dt_parts) > 0:
+                    published_dt = dt_parts[-1]
 
             parsed_dt = ''
             if published_dt:
+                published_dt = published_dt.replace('Updated: ', '').replace('.', ":")
                 try:
-                    parsed_dt = datetime.datetime.strptime(published_dt, '%b %d, %Y, %I.%M %p IST').isoformat()
+                    parsed_dt = parser.parse(published_dt).isoformat()
                 except:
                     raise Exception('Cannot parse datetime: %s' % published_dt)
 
-            body = content.xpath('//div[contains(@class, "article_content") or @itemprop="articleBody"]').extract_first()
+            body = response.xpath('//div[contains(@class, "article_content") or @itemprop="articleBody"]').extract_first()
 
             yield {'url': response.url, 'title': title, 'published_dt': parsed_dt, 'body': body}
